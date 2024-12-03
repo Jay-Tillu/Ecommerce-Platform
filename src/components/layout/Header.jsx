a241043a72aed2825291cf89fb932a222d4c0ca9
Jay Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2024-12-03 18:52:42 +0530

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Logo from "@/assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="bg-[#232F3E] text-white py-5">
       <nav className="flex items-center justify-between px-20 space-x-4">
-        <img className="w-28" src={Logo} alt="Logo" />
+        <Link to={"/"}>
+          <img className="w-28" src={Logo} alt="Logo" />
+        </Link>
         <ul className="flex flex-row space-x-8">
-          <li>Home</li>
-          <li>Cart</li>
-          <li>About</li>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/cart"}>Cart</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
         </ul>
       </nav>
     </header>
